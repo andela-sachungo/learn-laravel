3e1ae0a90a276cd42b505da1e14d483372060a27
andela-sachungo <sachungo1@gmail.com>
2015-09-25 18:05:56 +0300

    getting started with Bootstrap 3

--- dummy/resources/views/home/welcome.blade.php
+++ dummy/resources/views/home/welcome.blade.php
@@ -5,7 +5,7 @@
         <title>Welcome to TODOParrot</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <style>
             html, body {
                 height: 100%;
@@ -40,20 +40,15 @@
         <div class="container">
             <div class="content">
                <!--<div class="title">Laravel 5</div> -->
-               @extends('layouts.master')
-
-               @section('content')
-
-               <h1>Welcome to TODOParrot</h1>
-               @endsection
-
-               @section('advertisement')
-                   @parent
-                      <p>
-                          Buy the TODOParrot Productivity guide for $10!
-                      </p>
-                @endsection
+               <a href="http://www.wjgilmore.com" 
+                class="btn btn-success">W.J. Gilmore</a>
             </div>
         </div>
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </body>
 </html>
---
+++ dummy/resources/views/partials/row.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+	<title></title>
+</head>
+<body>
+<tr style = "padding-bottom: 5px;">
+	<td>
+		{{ $link ->name}}
+	</td>
+</tr>
+</body>
+</html>
